36366058a0db85d0ef9d4dcd1b2b5d71254e223c
Dhanishtha Bansal <dhanishtha.bansal@globallogic.com>
2018-08-03 12:53:54 +0530

    Added components and routing

--- CrudApplication/src/app/app.module.ts
+++ CrudApplication/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { UpdateComponent } from './update/update.component';
+import { DeleteComponent } from './delete/delete.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    UpdateComponent,
+    DeleteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
